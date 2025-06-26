83e95739996cba10e6da794a744d449525d40e15
anjali9472 <anjalisingh947299@gmail.com>
2025-06-26 12:38:04 +0530

    admin

---
+++ edit-doc.php
@@ -0,0 +1,74 @@
+
+    <?php
+    
+    
+
+    //import database
+    include("../connection.php");
+
+
+
+    if($_POST){
+        //print_r($_POST);
+        $result= $database->query("select * from webuser");
+        $name=$_POST['name'];
+        $nic=$_POST['nic'];
+        $oldemail=$_POST["oldcontact"];
+        $spec=$_POST['spec'];
+        $email=$_POST['contact'];
+        $tele=$_POST['Tele'];
+        $password=$_POST['password'];
+        $cpassword=$_POST['cpassword'];
+        $id=$_POST['id00'];
+        
+        if ($password==$cpassword){
+            $error='3';
+            $result= $database->query("select doctor.docid from doctor inner join webuser on doctor.doccontact=webuser.contact where webuser.contact='$contact';");
+            //$resultqq= $database->query("select * from doctor where docid='$id';");
+            if($result->num_rows==1){
+                $id2=$result->fetch_assoc()["docid"];
+            }else{
+                $id2=$id;
+            }
+            
+            echo $id2."jdfjdfdh";
+            if($id2!=$id){
+                $error='1';
+                //$resultqq1= $database->query("select * from doctor where doccontact='$contact';");
+                //$did= $resultqq1->fetch_assoc()["docid"];
+                //if($resultqq1->num_rows==1){
+                    
+            }else{
+
+                //$sql1="insert into doctor(doccontact,docname,docpassword,docnic,doctel,specialties) values('$contact','$name','$password','$nic','$tele',$spec);";
+                $sql1="update doctor set doccontact='$contact',docname='$name',docpassword='$password',docnic='$nic',doctel='$tele',specialties=$spec where docid=$id ;";
+                $database->query($sql1);
+                
+                $sql1="update webuser set contact='$contact' where contact='$oldcontact' ;";
+                $database->query($sql1);
+                //echo $sql1;
+                //echo $sql2;
+                $error= '4';
+                
+            }
+            
+        }else{
+            $error='2';
+        }
+    
+    
+        
+        
+    }else{
+        //header('location: signup.php');
+        $error='3';
+    }
+    
+
+    header("location: doctors.php?action=edit&error=".$error."&id=".$id);
+    ?>
+    
+   
+
+</body>
+</html>
